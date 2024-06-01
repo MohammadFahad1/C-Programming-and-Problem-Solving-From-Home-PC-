e607a14d5fa1bbd191a7de04f5a2dbbaace243a0
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-06-01 21:54:05 +0600

    Max Numbers Solved

---
+++ K_Max_Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<limits.h>
+
+int fun(int a[], int n, int i){
+    if(a[i] == n) return INT_MIN;
+    int max = fun(a, n, i+1);
+    printf("%d ", a[i]);
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    int max = fun(a, n, 0);
+    printf("%d\n", max);
+    return 0;
+}
